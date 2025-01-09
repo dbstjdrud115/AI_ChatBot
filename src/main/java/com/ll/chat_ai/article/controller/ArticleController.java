c29c8a4fdd23dcf8a63996a7e267707fc12e2824
Ojiner <dbstjdrud115@gmail.com>
2025-01-09 16:54:53 +0900

    JPA 심화 6강까지.

--- src/main/java/com/ll/chat_ai/article/controller/ArticleController.java
+++ src/main/java/com/ll/chat_ai/article/controller/ArticleController.java
@@ -2,6 +2,7 @@ package com.ll.chat_ai.article.controller;
 
 import com.ll.chat_ai.article.entity.Article;
 import com.ll.chat_ai.article.service.ArticleService;
+import groovy.util.logging.Slf4j;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -15,22 +16,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/article")
 @RequiredArgsConstructor
+@Slf4j
 public class ArticleController {
     private final ArticleService articleService;
+
     @GetMapping("/list")
     public String list(
-            @RequestParam(value = "page", defaultValue = "0") int page,
+            @RequestParam(value = "kwType", defaultValue = "") List<String> kwTypes,
+            @RequestParam(defaultValue = "") String kw,
+            @RequestParam(defaultValue = "0") int page,
             Model model
     ) {
+
         List<Sort.Order> sorts = new ArrayList<>();
         sorts.add(Sort.Order.desc("id"));
         Pageable pageable = PageRequest.of(page, 10, Sort.by(sorts));
-        Page<Article> itemsPage = articleService.search(pageable);
+
+        Map<String, Boolean> kwTypesMap = kwTypes
+                .stream()
+                .collect(Collectors.toMap(
+                        kwType -> kwType,
+                        kwType -> true
+                ));
+        //log.debug("kwTypesMap: {}", kwTypesMap);
+        System.out.println(kw);
+
+        Page<Article> itemsPage = articleService.search(kwTypes, kw, pageable);
+
         model.addAttribute("itemsPage", itemsPage);
+        model.addAttribute("kwTypesMap", kwTypesMap);
+
         return "article/list";
     }
 }
